cff42c26219cd16309bcb1a9168aa5e588692935
Randy <randylam87@gmail.com>
2017-04-24 06:39:14 -0700

    started html/css and a little js

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+var topics = [];
+
+$('.submit-btn').on('click',function(){
+	// topics.push($('#topic').val())
+	console.log(topics);
+	$(".form-control").trigger("reset");	
+})
+
+// app key dc6zaTOxFJmzC
